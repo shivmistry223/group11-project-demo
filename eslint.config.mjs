74c1c0e9b653256918dd51cfe18343191a9f525a
Shiv Mistry <shivmistry223@gmail.com>
2024-12-03 15:28:28 -0500

    lint fix

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,14 +6,4 @@ export default [
   { files: ["**/*.{js,mjs,cjs,jsx}"] },
   { languageOptions: { globals: globals.browser } },
   pluginReact.configs.flat.recommended,
-  {
-    plugins: {
-      react: pluginReact,
-    },
-    rules: {
-      "react/react-in-jsx-scope": "off",
-      "react/jsx-uses-react": "off",
-      "react/jsx-uses-vars": "warn",
-    },
-  },
 ];
