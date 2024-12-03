c31b6e36e2308f910ab2868d587e0e24793e2935
Shiv Mistry <shivmistry223@gmail.com>
2024-12-03 14:12:18 -0500

    integrated lint

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from "react";
 import "./App.css";
 import StudentForm from "./components/StudentForm/StudentForm";
 
--- src/App.test.js
+++ src/App.test.js
@@ -1,3 +1,4 @@
+import React from "react";
 import { render, screen, fireEvent, act } from "@testing-library/react";
 import App from "./App";
 
