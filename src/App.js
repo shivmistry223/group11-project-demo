f42b1708c86ffbd3f21d560c2f8872022e2e5eca
Shiv Mistry <shivmistry223@gmail.com>
2024-12-09 18:27:17 -0500

    updated header

--- src/App.js
+++ src/App.js
@@ -5,7 +5,6 @@ import StudentForm from "./components/StudentForm/StudentForm";
 function App() {
   return (
     <div className="App">
-      <h1>PROJECT Sagara </h1>
       <StudentForm />
     </div>
   );
